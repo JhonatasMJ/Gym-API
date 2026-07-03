9982b9cb518392222dad543bf077b7c456ec81cf
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-07-03 14:30:11 -0300

    feat/config env and validation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 //npm i typescript @types/node tsx tsup -D
 
-import { app } from "./app.js";
+import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
     host: "0.0.0.0",
-    port: 3333,
+    port: env.PORT,
 }).then (() => {
     console.log("HTTP Server running!");
 })
