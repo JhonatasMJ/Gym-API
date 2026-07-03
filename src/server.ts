dfd5395c7a385a98735b5eff839a52e4e0f1ba8e
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-07-03 14:41:27 -0300

    feat/config aliases

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-//npm i typescript @types/node tsx tsup -D
+// npm i typescript @types/node tsx tsup -D
+import { app } from './app'
+import { env } from './env'
 
-import { app } from "./app";
-import { env } from "./env";
-
-app.listen({
-    host: "0.0.0.0",
+app
+  .listen({
+    host: '0.0.0.0',
     port: env.PORT,
-}).then (() => {
-    console.log("HTTP Server running!");
-})
+  })
+  .then(() => {
+    console.log('HTTP Server running!')
+  })
