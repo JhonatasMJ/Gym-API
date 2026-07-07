0ac5259b3c776e41a486bed2eb611689e08f905e
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-07-07 14:53:03 -0300

    feat/config docker compose

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import fastify from 'fastify'
 import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+// docker ps -a - lista todos os containers
+// docker start (id) ou (nome) - inicia o container
+// docker rm (id) ou (nome) - remove o container
+// npx prisma migrate dev - cria as migrations com base no schema.prisma
+// npx prisma studio - abre o prisma studio para visualizar as tabelas e os dados
+// docker compose up -d - inicia o container do Postgres com o Docker Compose
 
 const prisma = new PrismaClient()
 prisma.user.create({
